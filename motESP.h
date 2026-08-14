93557822eb3ed0db975b6273092ccfe924f8eca0
Said <zumcitomc@gmail.com>
2026-08-14 16:26:17 -0600

    archivo .h de la librería motESP

--- motESP.h
+++ motESP.h
@@ -19,10 +19,12 @@ private:
 	const int resolution = 8;    // Resolución en bits (0-255 para 8 bits)
 	int velocidad;
 public:
-	void iniciarPWM(EN,CO1,CO2,int torque);
+	void iniciarPWM(int EN,int CO1,int CO2,int torque);
 	void iniciar(int EN, int CO1,int CO2);
 	void mover(char dir);
+	void moverPWM(char dir);
 	void alto();
-	void moverPWM();
+	void altoActivo();
+	void seleccionarVelocidad(int valor);
 };
 #endif

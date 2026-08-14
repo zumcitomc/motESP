b5f731a23e06ac1712056693ba037d05bb4b6add
zumcitomc <zumcitomc@gmail.com>
2026-08-14 11:08:38 -0600

    motESP es una librería para controlar motores dc con L298N y esp32

---
+++ motESP.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#ifndef motESP
+#define motESP
+
+//hey, si estas leyendo esto, preparate para el mucho texto :)
+class motesp{
+private:
+	int EN;
+	int CO1;
+	int CO2;
+	int en;
+	int co1;
+	int co2;
+	const int ledChannel = 0;
+	const int ledChael = 1;
+	const int ledChanel = 2;
+	const int frequency = 5000;  // Frecuencia en Hz
+	const int resolution = 8;    // Resolución en bits (0-255 para 8 bits)
+	int velocidad;
+public:
+	void iniciarPWM(EN,CO1,CO2,int torque);
+	void iniciar(int EN, int CO1,int CO2);
+	void mover(char dir);
+	void alto();
+	void moverPWM();
+};
+#endif
